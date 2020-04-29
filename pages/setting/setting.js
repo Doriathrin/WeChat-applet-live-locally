d22b865fcb9daa343c4d25bc2da64f7b9d92174e
Lionheart <1282420341@qq.com>
2020-04-29 08:19:27 +0800

    live locally

--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -1,30 +1,31 @@
-// pages/profile/profile.js
+// pages/setting/setting.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo:null,
+    switch1Checked:true,
+    switch2Checked:true
   },
-  getUserInfo(e){
-    console.log(e);
+  switch2Checked(){
+    var switch2Checked=!this.data.switch2Checked
     this.setData({
-      userInfo:e.detail.userInfo
+      switch2Checked
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
